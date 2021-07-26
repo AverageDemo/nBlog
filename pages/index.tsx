6ffb775ab83c17fedd862110516911c49e3aaa13
Average Demo <46252711+AverageDemo@users.noreply.github.com>
2021-07-26 01:17:10 -0400

    Setting up database interaction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Post, PrismaClient } from '@prisma/client';
 
-export default function Home() {
+import styles from '../styles/Home.module.css';
+import moment from 'moment';
+
+export default function Home({ posts }: { posts: Post[] }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,44 +15,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              {post.title} by {post.author} on {moment(post.createdAt).format('MMMM Do YYYY')}
+            </li>
+          ))}
+        </ul>
       </main>
 
       <footer className={styles.footer}>
@@ -65,5 +37,14 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const prisma = new PrismaClient();
+  const posts: Post[] = await prisma.post.findMany();
+
+  return {
+    props: { posts },
+  };
 }
