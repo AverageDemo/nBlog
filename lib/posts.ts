559bae6454c66323136c6e5ebd367733aaea881a
Average Demo <46252711+AverageDemo@users.noreply.github.com>
2021-07-29 01:15:33 -0400

    Error handling

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,3 +1,5 @@
+import { Prisma } from '@prisma/client';
+
 import prisma from '@/lib/prisma';
 import { Post } from '@prisma/client';
 import NewPostDto from '@/dtos/new-post.dto';
@@ -29,19 +31,32 @@ export const getPostBySlug = async (slug: string) => {
   return post;
 };
 
-export const createPost = async (newPostDto: NewPostDto): Promise<Post | null> => {
-  const post: Post = await prisma.post.create({
-    data: {
-      cid: newPostDto.cid,
-      tags: newPostDto.tags,
-      title: newPostDto.title,
-      slug: newPostDto.slug,
-      content: newPostDto.content,
-      authorId: newPostDto.authorId,
-    },
-  });
+export const createPost = async (newPostDto: NewPostDto): Promise<Post | object | null> => {
+  try {
+    const post: Post = await prisma.post.create({
+      data: {
+        cid: newPostDto.cid,
+        tags: newPostDto.tags,
+        title: newPostDto.title,
+        slug: newPostDto.slug,
+        content: newPostDto.content,
+        authorId: newPostDto.authorId,
+      },
+    });
 
-  if (post) return post;
+    if (post) return post;
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === 'P2002') {
+        return {
+          status: 500,
+          message: 'A post with this title already exists',
+        };
+      }
+
+      throw new Error(e.message);
+    }
+  }
 
   return null;
 };
