9a61873a725c5cd5e225ac8e31c7d186e701e1ba
Average Demo <46252711+AverageDemo@users.noreply.github.com>
2021-07-29 04:44:11 -0400

    Index design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import moment from 'moment';
+import Link from 'next/link';
 import type { GetStaticProps } from 'next';
+import moment from 'moment';
 import type { Post } from '@prisma/client';
 
 import { getPosts } from '@/lib/posts';
@@ -10,20 +11,49 @@ export default function Home({ posts }: Props) {
   return (
     <Layout>
       <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-        <h1 className="text-3xl font-bold text-gray-900 tracking-tight sm:text-4rem md:text-6xl md:leading-10">
+        <h1 className="text-3xl font-bold text-gray-900 tracking-tight sm:text-4rem md:text-7xl md:leading-10">
           Latest
         </h1>
-        <p className="text-lg text-gray-500">All the latest posts</p>
+        <p className="text-lg pt-4 text-gray-500">All the latest posts</p>
       </div>
       <ul className="divide-y divide-gray-200">
         {posts.length ? (
           posts.map((post) => (
-            <li key={post.id}>
-              {post.title} by {post.author.username} on {moment(post.createdAt).format('MMMM Do YYYY')}
+            <li key={post.id} className="py-12">
+              <article className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
+                <dl>
+                  <dt className="sr-only">Published on</dt>
+                  <dd className="text-base font-medium text-gray-500">
+                    <time dateTime="2021-06-17T14:00:00.000Z">{moment(post.createdAt).format('MMMM Do, YYYY')}</time>
+                  </dd>
+                </dl>
+                <div className="space-y-5 xl:col-span-3">
+                  <div className="space-y-6">
+                    <h2 className="text-2xl font-bold tracking-tight">
+                      <a className="text-gray-900" href="/tailwindcss-2-2">
+                        {post.title}
+                      </a>
+                    </h2>
+                    <div className="prose max-w-none text-gray-500">
+                      <p>
+                        {post.content.substr(0, 450)}
+                        {post.content.length > 450 && '...'}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="text-base font-medium">
+                    <Link href="#">
+                      <a className="text-teal-600 hover:text-teal-700" aria-label={`Read "${post.title}"`}>
+                        Read more &rarr;
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+              </article>
             </li>
           ))
         ) : (
-          <li>No posts to display</li>
+          <li className="py-12">No posts to display yet</li>
         )}
       </ul>
     </Layout>
