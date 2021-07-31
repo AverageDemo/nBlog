5cc0c7875b779acfa929b93c719cdadc29bcf3bd
Average Demo <46252711+AverageDemo@users.noreply.github.com>
2021-07-30 21:03:33 -0400

    Added auth provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Provider } from 'next-auth/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { session } = pageProps;
+
+  return (
+    <Provider session={session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 export default MyApp;
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,21 +1,15 @@
 import { useRouter } from 'next/router';
 import { useSession, getSession } from 'next-auth/client';
+import Layout from '@/components/Layout';
 
 export default function DashboardIndexPage() {
   const router = useRouter();
   const [session, loading] = useSession();
-
-  if (loading) return null;
 
   if (!loading && !session) {
     router.push('/login');
     return null;
   }
 
-  return (
-    <>
-      <h1>Protected Page</h1>
-      <p>You can view this page because you are signed in.</p>
-    </>
-  );
+  return <Layout>Test</Layout>;
 }
