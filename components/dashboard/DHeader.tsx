92d30355c1b745702d99bc5b5bb2cfdd26c04f0d
Average Demo <46252711+AverageDemo@users.noreply.github.com>
2021-07-31 02:35:08 -0400

    Remove "New Post" from the New Post page

--- components/dashboard/DHeader.tsx
+++ components/dashboard/DHeader.tsx
@@ -1,18 +1,24 @@
-import { navigation } from '@/lib/navigation';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import { navigation } from '@/lib/navigation';
 
 export default function DHeader({ title }: Props) {
+  const router = useRouter();
+
   return (
     <div className="border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
       <div className="flex-1 min-w-0">
         <h1 className="text-lg font-medium leading-6 text-gray-900 sm:truncate">{title}</h1>
       </div>
       <div className="mt-4 flex sm:mt-0 sm:ml-4">
-        <Link href={navigation.dashboard.newPost.href}>
-          <a className="order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 sm:ml-3">
-            New Post
-          </a>
-        </Link>
+        {router.pathname !== navigation.dashboard.newPost.href && (
+          <Link href={navigation.dashboard.newPost.href}>
+            <a className="order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 sm:ml-3">
+              New Post
+            </a>
+          </Link>
+        )}
       </div>
     </div>
   );
